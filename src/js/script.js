8bc09a45dd7411e5134e58c15b2b682b2576d2ca
Veloza Juan <juan.veloza_arcila@edu.univ-fcomte.fr>
2024-12-17 00:13:11 +0100

    Mise en forme du landing page et creation de cards et du footer

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,11 @@ const nav = document.querySelector(".menu");
 const page = document.body;
 
 if (toggle && nav) {
-toggle.addEventListener("click", () => {
-    const isOpen = toggle.ariaExpanded === "true";
+    toggle.addEventListener("click", () => {
+    const isOpen = toggle.getAttribute("aria-expanded") === "true";
     const isClosed = !isOpen;
-    toggle.ariaExpanded = isClosed;
-    nav.ariaHidden = isOpen;
+    toggle.setAttribute("aria-expanded", isClosed);
+    nav.setAttribute("aria-hidden", isOpen);
     page.classList.toggle("noscroll", isClosed);
-});
+    });
 }
