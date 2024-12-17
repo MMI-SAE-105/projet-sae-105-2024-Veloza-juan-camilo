1b9aa7bc560d13830f6a88ffc22fe07ef9c8cd47
Veloza Juan <juan.veloza_arcila@edu.univ-fcomte.fr>
2024-12-17 19:38:53 +0100

    Mise en forme du contenu de la page web : Page d'accueil, page Crispr et page livre

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
-const toggle = document.querySelector(".menu-btn");
-const nav = document.querySelector(".menu");
-const page = document.body;
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.querySelector(".menu-btn");
+    const nav = document.querySelector(".menu");
+    const page = document.body;
 
-if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-    const isOpen = toggle.getAttribute("aria-expanded") === "true";
-    const isClosed = !isOpen;
-    toggle.setAttribute("aria-expanded", isClosed);
-    nav.setAttribute("aria-hidden", isOpen);
-    page.classList.toggle("noscroll", isClosed);
-    });
-}
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            const isOpen = toggle.getAttribute("aria-expanded") === "true";
+            toggle.setAttribute("aria-expanded", !isOpen);
+            nav.setAttribute("aria-hidden", isOpen);
+            page.classList.toggle("noscroll", !isOpen);
+        });
+    }
+});
